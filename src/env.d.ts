c1d38eb85dbc70fd1b2d0ce538e217a0f7b864bf
gabechu <mbudak21@ku.edu.tr>
2025-11-01 00:33:53 +0300

    Modularized Background3D

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,16 @@
 /// <reference types="astro/client" />
 /// <reference types="vite/client" />
+
+export {};
+
+declare global {
+  interface Window {
+    Background3D: {
+      getSpeedx(): number;
+      setSpeedx(spd: number): void;
+      getSpeedy(): number;
+      setSpeedy(spd: number): void;
+      setColor(hex: import('three').ColorRepresentation): void;
+    };
+  }
+}
