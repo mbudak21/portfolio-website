1cd383e18a063d22e0c5962c883163d0816c800d
gabechu <mbudak21@ku.edu.tr>
2025-11-04 19:10:35 +0300

    Added initial locale support

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,12 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.mbudak21.com",
+  i18n: {
+    locales: ["en", "tr"], //, "ru", "zh-CN", "sv"
+    defaultLocale: "en",
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  }
+
 });
---
+++ i18n/ui.ts
@@ -0,0 +1,29 @@
+export const languages = {
+  en: 'English',
+  tr: 'Turkish',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'test': '8_8',
+    'pageTitle': '~/portfolio',
+    'homePage.h1': 'Clean Code. Systems. Graphics.',
+    'homePage.h1.p': 'I build fast, minimal software and occasional pretty pixels.',
+    'homePage.GlassButtonText': 'View Projects',
+    'homePage.About': 'About',
+    'homePage.About.p1': 'Every few months I wonder if I even like programming, or if I just hate not understanding things I’ll never use.',
+    'homePage.About.p2': 'Then I spend four hours debugging why my latest wayland config got nuked.',
+    'homePage.About.p3': 'Maybe that’s what passion is. Or maybe it’s brain damage. Hard to say.',
+    'homePage.About.p4': 'The rest of the website is in development, I’ll push it in a few days.'
+
+
+
+  },
+  tr: {
+    'test': 'Merhaba',
+    'pageTitle': '~/portfolyo',
+    'homePage.h1': 'tbd'
+  },
+} as const;
